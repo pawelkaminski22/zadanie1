73038a88457f19e24b3bb6b08091068b3e6fd90e
Pawel Kaminski <pawelkaminski22@gmail.com>
2021-01-16 17:39:10 +0100

    Adding final version

--- greetings/views.py
+++ greetings/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def greetings(request):
+    return HttpResponse("Hello World from views")
+
+
+def random_name(request, name):
+    return HttpResponse(f'Hello ' + name.capitalize() + f'!')
--- zadanie1/urls.py
+++ zadanie1/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from greetings.views import greetings, random_name
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('greetings/', greetings),
+    path('greetings/<name>/', random_name)
 ]
